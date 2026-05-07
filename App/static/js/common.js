5a72005dd7a7d9de7bc3c3d2f26e471695014ee2
haru-02-code <gakkouyou0225@gmail.com>
2026-05-07 17:13:11 +0900

    js追加

--- App/static/js/common.js
+++ App/static/js/common.js
@@ -1,1 +1,15 @@
 //jQuery設定ファイル
+$(function () {
+  // 初期色設定
+  $('.color-btn').each(function () {
+    const color = $(this).data('color');
+    $(this).css('background-color', color);
+  });
+
+  $('.color-btn').on('click', function () {
+
+    $('.color-btn').removeClass('active');
+    
+    $(this).addClass('active');
+  });
+});
